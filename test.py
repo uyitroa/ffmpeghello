edbbf75d3b653070f66536f41913124c2691445b
shintaroDesu <snkraishin87@gmail.com>
2020-08-11 22:40:56 +0200

    ez

--- test.py
+++ test.py
@@ -2,5 +2,10 @@ import pymyarray2
 import numpy as np
 arr = pymyarray2.PyMyArray(10)
 print(arr)
-nparr = np.asarray(arr, np.uint8)
+#nparr = np.asarray(arr, np.uint8)
+nparr = np.frombuffer(arr, dtype=np.uint8)
+print(nparr[:10])
+
+nparr[1] = 100
+print(arr)
 print(nparr[:10])
